60684ee098bb65c9a010f91231830ab4559916f1
nikhilsampangi <nikhil.sampangi@gmail.com>
2019-11-24 07:06:30 +0530

    Inputs done

---
+++ Auction.py
@@ -0,0 +1,16 @@
+def Input():
+    # Part - 1 : Taking Inputs
+    print("\nNetwork Optimimzation Programming Assignment #3")
+    print("\nAuction Algorithm")
+    n = int(input("\nNo of Persons :"))
+    m = int(input("\nNo of Objects :"))
+    print("\nEnter the Benefit(aij) martix Aij :")
+    Aij = []
+    for i in range(n):
+        Aij.append([int(x) for x in input().split()])
+        if len(Aij[i]) != m:
+            print("\nError: Expected ", m, " values in a row")
+            break
+
+
+Input()
